9f9fd3a4785d4ac18417fcc1267cbe014e8a771e
Sergey Antonov <flyker777@yandex.ru>
2019-10-30 20:30:45 +0300

    add views and layouts

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,29 @@
 <template>
   <div id="app">
-    <router-view />
+    <component :is="layout">
+      <router-view />
+    </component>
   </div>
 </template>
+
+<script>
+import authLayout from '@/layouts/authLayout.vue';
+import mainLayout from '@/layouts/mainLayout.vue';
+
+export default {
+  computed: {
+    layout() {
+      return (this.$route.meta.layout || 'auth') + '-layout';
+    }
+  },
+  components: {
+    authLayout,
+    mainLayout
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "~materialize-css/dist/css/materialize.min.css";
+  @import "./assets/index.css";
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
@@ -8,7 +7,56 @@ const routes = [
   {
     path: "/",
     name: "home",
-    component: Home
+    meta: { layout: "main" },
+    component: () => import("../views/Home.vue")
+  },
+  {
+    path: "/login",
+    name: "login",
+    meta: { layout: "auth" },
+    component: () => import("../views/Login.vue")
+  },
+  {
+    path: "/register",
+    name: "register",
+    meta: { layout: "auth" },
+    component: () => import("../views/Register.vue")
+  },
+  {
+    path: "/categories",
+    name: "categories",
+    meta: { layout: "main" },
+    component: () => import("../views/Categories.vue")
+  },
+  {
+    path: "/details",
+    name: "details",
+    meta: { layout: "main" },
+    component: () => import("../views/DetailRecords.vue")
+  },
+  {
+    path: "/history",
+    name: "history",
+    meta: { layout: "main" },
+    component: () => import("../views/History.vue")
+  },
+  {
+    path: "/planning",
+    name: "planning",
+    meta: { layout: "main" },
+    component: () => import("../views/Planning.vue")
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    meta: { layout: "main" },
+    component: () => import("../views/Profile.vue")
+  },
+  {
+    path: "/records",
+    name: "records",
+    meta: { layout: "main" },
+    component: () => import("../views/Records.vue")
   }
 ];
 
