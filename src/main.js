e64e4f1dd26e0997668f20c5c8c018448e48791c
Sergey Antonov <flyker777@yandex.ru>
2019-10-31 16:05:36 +0300

    fin logout btn

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import dateFilter from "@/filters/date.filter";
+import messagePlugin from "@/utils/message.plugin.js";
 import "materialize-css/dist/js/materialize.min.js";
 
 Vue.config.productionTip = false;
 
+Vue.use(messagePlugin);
 Vue.use(Vuelidate);
 Vue.filter("date", dateFilter);
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -33,6 +33,7 @@
 
 <script>
 import { email, required, minLength } from "vuelidate/lib/validators";
+import messages from '@/utils/messages.js';
 
 export default {
   name: "login",
@@ -50,6 +51,11 @@ export default {
     password: {
       required,
       minLength: minLength(6)
+    }
+  },
+  mounted() {
+    if (messages[this.$route.query.message]) {
+      this.$message(messages[this.$route.query.message]);
     }
   },
   methods: {
