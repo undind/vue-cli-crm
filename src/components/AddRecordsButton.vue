0b09aee332596d7ac088e3944b8fb6a932f2d969
Sergey Antonov <flyker777@yandex.ru>
2019-10-31 14:18:21 +0300

    fin sidebar, navbar links and date-time reload

---
+++ src/components/AddRecordsButton.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="fixed-action-btn">
+    <router-link class="btn-floating btn-large blue" to="/records">
+      <i class="large material-icons">add</i>
+    </router-link>
+  </div>
+</template>
+
